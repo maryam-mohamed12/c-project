dd80722c76f6997b96b057d8982563e5f17123a3
maryam-mohamed12 <117026401+maryam-mohamed12@users.noreply.github.com>
2022-12-16 18:09:44 +0200

    Update test1.cs

--- test1.cs
+++ test1.cs
@@ -1,12 +1,12 @@
 using System;
 namespace OopSection;
-
 public abstract class Person{
   public string Name;
   public int Age;
-  public Person(string name;int age){
-    Name=name;
-    Age=age;
+
+  public Person(string name,int age){
+    this.Name=name;
+    this.Age=age;
   }
   public abstract void Print();
 }
@@ -14,41 +14,47 @@ public class Student :Person{
   public int Year;
   public float Gpa;
   public Student(string name,int age, int year, float gpa):base(name,age){
-    Year=year;
-    Gpa=gpa;
+    this.Year=year;
+    this.Gpa=gpa;
   }
   public override void Print(){
-    Console.WriteLine($"My name is{Name} , My age is{Age} , and my GPA is {Gpa}");
+    Console.WriteLine($"My name is {Name} , My age is s{Age} , and my GPA is {Gpa}");
 
   }
 }
 public class Database{
+
   int current_index;
   public Person[] People =new Person[50];
+
   public void AddStudent(Student student){
     People[current_index++]=student; 
   }
+
   public void AddStaff(Staff staff){
     People[current_index++]= staff;
   }
+
   public void PrintAll(){
-    for (int i = 0; i <= current_index; i++)
+    for (int i = 0; i < current_index; i++)
     {
       People[i].Print();
     }
   }
 }
 public class Staff :Person{
-  public double Salary;
+  public int Salary;
   public int JoinYear;
 
-  public Staff(double salary , int joinyear,string name,int age):base(name,age){
-    Salary=salary;
-    JoinYear=joinyear;
+  public Staff(string name,int age,int salary ,int joinyear):base(name,age){
+    this.Salary=salary;
+    this.JoinYear=joinyear;
+
   }
   public override void Print(){
-    Console.WriteLine($"My name is{Name} , My age is{Age} , and my salary is {Salary}");
+    Console.WriteLine($"My name is {Name} , My age is {Age} , and my salary is {Salary}");
   }
+
 }
 public class Program
 {
@@ -58,7 +64,7 @@ public class Program
       var database =new Database();
       while (true)
       {
-        Console.WriteLine("1-student \n 2-staff \n 3-print all");
+        Console.WriteLine("1-staff \n 2-student \n 3-print all");
         Console.WriteLine("option :");
         var option =Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -67,27 +73,27 @@ public class Program
               Console.Write("Name: ");
               var name =Console.ReadLine();
               Console.Write("Age: ");
-              var age = convert.ToInt32(Console.ReadLine());
+              var age = Convert.ToInt32(Console.ReadLine());
               Console.Write("Salary: ");
-              var salary = convert.ToDouble(Console.ReadLine());
+              var salary = Convert.ToInt32(Console.ReadLine());
               Console.Write("Join year :");
-              var joinyear = convert.ToInt32(Console.ReadLine());
-              var student = new Student(name,age,salary,joinyear);
-              database.AddStudent(student);
+              var joinyear = Convert.ToInt32(Console.ReadLine());
+              var staff = new Staff(name,age,salary,joinyear);
+              database.AddStaff(staff);
               break;
 
 
           case 2:
               Console.Write("Name: ");
-              var name =Console.ReadLine();
+              var name2 =Console.ReadLine();
               Console.Write("Age: ");
-              var age = convert.ToInt32(Console.ReadLine());
+              var age2 = Convert.ToInt32(Console.ReadLine());
               Console.Write("Year: ");
-              var year = convert.ToInt32(Console.ReadLine());
+              var year = Convert.ToInt32(Console.ReadLine());
               Console.Write("GPA :");
-              var gpa = convert.ToSingle(Console.ReadLine());
-              var student = new Student(name,age,year,gpa);
-              database.AddStaff(staff);
+              var gpa = Convert.ToSingle(Console.ReadLine());
+              var student = new Student(name2,age2,year,gpa);
+              database.AddStudent(student);
             break;
 
 
